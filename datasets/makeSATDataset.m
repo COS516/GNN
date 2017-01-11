380b77f117fe09ce7ad6ae5d0b4a46d035527dd0
Daniel Suo <danielsuo@gmail.com>
2017-01-11 15:15:34 -0500

    Let's just roll with it

---
+++ datasets/makeSATDataset.m
@@ -0,0 +1,7 @@
+function makeSATDataset(numFormulae, numVariables, k, numClauses, numTrain, numTest, numValidate)
+
+global dataSet
+
+dataSet = DataSet(numFormulae, numVariables, k, numClauses, numTrain, numTest, numValidate);
+
+end
